21e160009d150c1702fa427b38ea3c05934477ff
navthukral <navthukral180504@gmail.com>
2024-08-22 14:35:53 +0530

    Installed Packages

---
+++ code/code.R
@@ -0,0 +1,29 @@
+# Install packages
+install.packages("tidyverse")
+install.packages("caret")
+install.packages("GGally")
+install.packages("repr")
+install.packages("infer")
+install.packages("cowplot")
+install.packages("broom")
+install.packages("modelr")
+install.packages("car")
+install.packages("pROC")
+
+# Load packages
+suppressPackageStartupMessages({
+  library(tidyverse)  # Loads dplyr, tidyr, readr, ggplot2, and more
+  library(caret)
+  library(GGally)
+  library(repr)
+  library(infer)
+  library(cowplot)
+  library(broom)
+  library(modelr)
+  library(car)
+  library(pROC)
+  library(readxl)
+})
+
+
+
